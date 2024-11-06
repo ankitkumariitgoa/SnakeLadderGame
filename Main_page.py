ead389fb10fa5962a9f9bbefa4dfbbf0c43129a6
ankitkumariitgoa <ankit12556070kumar@gmail.com>
2024-11-06 20:00:51 +0530

    Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -11,44 +11,37 @@ st.set_page_config(
     page_title="Project",
     layout='wide'
 )
-class MultiApp:
-    def __init__(self) -> None:
-        self.apps=[]
-    def add_app(self,title,func):
-        self.apps.append({
-            'title':title,
-            'function':func
-        })
-    def run():
-        with st.sidebar:
-            app=option_menu(
-                menu_title='Project',
-                options=['🏠Home','🐍Game','❓About Us❔',"📚Rule Book",'🛜Resources Used/Referred'],
-                # icons=['a','b','c','d']
-                default_index=0
-            )
-        if app=='🏠Home':
-            Home.run()
-        if app=='🐍Game':
-            # with st.sidebar:
-            apps=option_menu(
-                menu_title='Snake And Ladder',
-                options=["🐍Player vs Player",'🐍Player vs Computer'],
-                # icons=['a','b']
-                default_index=0,
-                orientation='horizontal'
-            )
-            if apps=='🐍Player vs Player':
-                snake_ladder_1.run()
-            if apps=='🐍Player vs Computer':
-                practice.run()
-                
-        
 
-        if app=='❓About Us❔':
-            About_us.run()
-        if app=='🛜Resources Used/Referred':
-            resources.run()
-        if app=="📚Rule Book":
-            rules.run()
-    run()
+def run():
+    with st.sidebar:
+        app=option_menu(
+            menu_title='Project',
+            options=['🏠Home','🐍Game','❓About Us❔',"📚Rule Book",'🛜Resources Used/Referred'],
+            # icons=['a','b','c','d']
+            default_index=0
+        )
+    if app=='🏠Home':
+        Home.run()
+    if app=='🐍Game':
+        # with st.sidebar:
+        apps=option_menu(
+            menu_title='Snake And Ladder',
+            options=["🐍Player vs Player",'🐍Player vs Computer'],
+            # icons=['a','b']
+            default_index=0,
+            orientation='horizontal'
+        )
+        if apps=='🐍Player vs Player':
+            snake_ladder_1.run()
+        if apps=='🐍Player vs Computer':
+            practice.run()
+            
+    
+
+    if app=='❓About Us❔':
+        About_us.run()
+    if app=='🛜Resources Used/Referred':
+        resources.run()
+    if app=="📚Rule Book":
+        rules.run()
+run()
